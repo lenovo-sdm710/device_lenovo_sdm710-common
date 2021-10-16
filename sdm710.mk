9d724270f36d0cc12ae9f3c4620460c4124125c6
lzgmc <lazyguymc@lzgmc.tk>
2021-10-16 15:31:18 -0700

    sdm710-common: Build whole soundtrigger
    When Voice Match is enabled in Google settings, it causes issues with phone
    calls, as reported in PixelExperience/android-issues#858. Build whole
    soundtrigger to disable hotword enrollment.

--- sdm710.mk
+++ sdm710.mk
@@ -73,7 +73,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.2-impl:32 \
+    android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
